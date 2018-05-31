52ff08000362677aa1e334a817e4992946e88b81
hjkws <755201617@qq.com>
2018-05-31 11:52:34 +0800

    jks

---
+++ promiseall用法.js
@@ -0,0 +1,23 @@
+let p1 = new Promise((resolve,reject)=>{
+    let obj1={"aaa":111}
+    setTimeout(()=>{
+
+        resolve({"obj1":obj1})
+    },500)
+})
+let p2 = new Promise((resolve,reject)=>{
+    let obj2={"bbb":222}
+    setTimeout(()=>{
+
+        resolve({"obj2":obj2})
+    },1000)
+})
+
+const p = Promise.all([p1, p2]).then(arr=>{
+    var obj = {};
+    for([index,value] of arr.entries()){
+        Object.assign(obj,value);
+    }
+    console.log(JSON.stringify(obj));
+   
+})
